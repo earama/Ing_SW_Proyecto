88b5cd2812ace3af314186b5c64e7cd142265e0b
Sebastian Alcala <sebastianalcalagebran@gmail.com>
2019-05-19 23:53:07 -0400

    Commit 5 (Sebastian Alcala)
    Implementaremos el uso del Input System.

---
+++ FireFinger/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+
+public class Player : MonoBehaviour
+{
+    public InputMaster controls;
+
+    private void Awake() 
+    {
+        controls.Player.Shoot.performed += ctx => Shoot();
+    }
+
+    void Shoot ()
+    {
+        Debug.Log("We shot!");
+    }
+
+    private void OnEnable() {
+        controls.Enable();
+    }
+    private void OnDisable() {
+        controls.Disable();
+    }
+}
