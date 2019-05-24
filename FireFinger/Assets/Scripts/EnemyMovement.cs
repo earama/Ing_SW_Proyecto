abc7b1f793584f2c213e0d33ce6b4c89bb1fd418
Sebastian Alcala <sebastianalcalagebran@gmail.com>
2019-05-24 01:19:56 -0400

    Commit 9
    Implementacion de Movimiento tactil, particulas de movimiento, limites de mapa, enemigo, movimiento de enemigo, efectos de movimiento de enemigo.

---
+++ FireFinger/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 3;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update() 
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed* Time.deltaTime);
+        Debug.Log(target.position);
+    }
+}
