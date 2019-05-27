eb12f12bbc093d3f5f1879c97233fa0509f95bcc
Sebastian Alcala <sebastianalcalagebran@gmail.com>
2019-05-27 15:32:58 -0400

    commit x
    update

--- FireFinger/Assets/Scripts/Projectile.cs
+++ FireFinger/Assets/Scripts/Projectile.cs
@@ -7,20 +7,20 @@ public class Projectile : MonoBehaviour
     public int damage = 10;
     public Rigidbody2D rb;
     public float speed = 10f;
-    // private Vector2 screenBounds;
+    private Vector2 screenBounds;
 
-    // Start is called before the first frame update]
+    // Start is called before the first frame update
     private void Start() 
     {
-        // screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
     void Update()
     {  
         rb.velocity = transform.up * speed;
-        // if(transform.position.x >= screenBounds.x || transform.position.y >= screenBounds.y)
-        // {
-        //     gameObject.SetActive(false);
-        // }
+        if(transform.position.x >= screenBounds.x || transform.position.y >= screenBounds.y)
+        {
+            gameObject.SetActive(false);
+        }
     }
     
     void OnTriggerEnter2D(Collider2D hitInfo) 
