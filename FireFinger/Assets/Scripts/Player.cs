dc0bc59b111bf38c1560113ae4eefda1d4408485
earama <43919668+earama@users.noreply.github.com>
2019-06-03 17:41:46 -0400

    Sistema de Vidas

---
+++ FireFinger/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Este script va en el player
+public class Player : MonoBehaviour
+{
+    public GameObject healthPrefab;
+    public int maxLives;
+
+    private List<GameObject> lives;
+    private Vector2 screenBounds; // screen limits
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set screen limits
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        // Set lives
+        lives = new List<GameObject>();
+        for(int i = 0; i < maxLives; i++){
+            GameObject curLife = Instantiate(healthPrefab) as GameObject;
+            // Get dimensions
+            var heartWidth = curLife.GetComponent<SpriteRenderer>().bounds.size.x;
+            var heartHeight = curLife.GetComponent<SpriteRenderer>().bounds.size.y;
+            // Give position
+            Vector2 pos = new Vector2(-screenBounds.x + heartWidth/2*(i+1), screenBounds.y-heartHeight/2); 
+            Debug.Log(pos);
+            curLife.transform.position = new Vector2(-screenBounds.x + heartWidth*(i+1), screenBounds.y-heartHeight);
+            // Add to list
+            lives.Add(curLife);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeHit(int hits) //enemy collides with projectile
+    {
+        for(int i = 0; i < hits; i++){
+            // Removes rightmost life
+            lives.RemoveAt(lives.Count-1);
+        }
+
+        if(lives.Count == 0)
+        {
+            Die(); //player dies
+        }        
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject); //player game object gets deleted
+    }
+}
